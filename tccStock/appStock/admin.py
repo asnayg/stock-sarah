6ec56923a353a7fd41d7d223ed9d057ee544106e
asnayg <asnayg@gmail.com>
2018-12-22 19:26:41 -0300

    update 1

--- tccStock/appStock/admin.py
+++ tccStock/appStock/admin.py
@@ -1,7 +1,49 @@
 # Register your models here.
 
 from django.contrib import admin
+from .models import Producto, OrdenSalida, OrdenEntrada, OrdenDevolucion
 
-from .models import Producto
+admin.site.index_title = "Sistema de Stock de TCC"
+admin.site.site_header = "Stock System"
+admin.site.site_title = "TCC Stock Admin Portal"
 
-admin.site.register(Producto)
+#Elimina la opcion de eliminar una entidad desde el admin.
+admin.site.disable_action('delete_selected')
+
+
+class OrdenEntradaInline(admin.StackedInline):
+    model = OrdenEntrada
+
+
+class ProductoAdmin(admin.ModelAdmin):
+     fieldsets = [
+         ('Datos del Producto',                 {'fields': ['identificador', 'marca', 'modelo', 'estado','ubicacion']}),
+         ('Observaciones', {'fields': ['observacion'], 'classes': ['collapse']}),
+     ]
+     inlines = [OrdenEntradaInline]
+     list_display = ('identificador', 'estado', 'cantidad', 'ubicacion' , 'fecha_actualizacion')
+     list_per_page = 50
+     readonly_fields = ["cantidad"]
+     list_filter = ("estado", "ubicacion", "fecha_actualizacion",)
+     list_display_links = None
+
+
+class OrdenSalidaAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'estado', 'cantidad_producto', 'producto' , 'fecha_creacion')
+    list_per_page = 50
+    list_filter = ("estado", "cantidad_producto", 'fecha_creacion')
+    #actions = ['delete_selected']
+    #actions = None
+
+
+class OrdenDevolucionAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'cantidad_producto', 'orden_salida', 'fecha_creacion')
+    list_per_page = 50
+    list_filter = ("cantidad_producto", 'fecha_creacion')
+    #actions = ['delete_selected']
+
+
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(OrdenSalida, OrdenSalidaAdmin)
+admin.site.register(OrdenDevolucion, OrdenDevolucionAdmin)
+#admin.site.register(OrdenEntrada)
--- tccStock/appStock/models.py
+++ tccStock/appStock/models.py
@@ -1,10 +1,101 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 
 class Producto(models.Model):
-    nombre = models.CharField(max_length=200)
-    update_date = models.DateTimeField('date update')
+    ESTADOS = (
+        ('S', 'Stock'),
+        ('B', 'BackUp'),
+        ('U', 'En USO'),
+    )
+    identificador = models.CharField(max_length=200, unique=True )
+    marca = models.CharField(max_length=100)
+    modelo = models.CharField(max_length=200)
+    estado = models.CharField(max_length=1, choices=ESTADOS)
+    ubicacion = models.CharField(max_length=200, default="Deposito")
+    observacion = models.TextField(blank=True)
+    fecha_actualizacion = models.DateField(editable=False, default=datetime.date.today)
+    cantidad = models.PositiveIntegerField(editable=False, default=0)
 
     def __str__(self):
-        return self.nombre
+        return self.identificador
+
+    def actualizarCantidad(self, cant):
+        self.cantidad += cant
+        super().save()
+
+    class Meta:
+        ordering = ["fecha_actualizacion"]
+        verbose_name_plural = "Productos"
+
+
+class Orden(models.Model):
+    numero = models.CharField(max_length=100, unique=True)
+    fecha_creacion = models.DateField()
+    cantidad_producto = models.PositiveIntegerField(default=0)
+    observacion = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.observacion
+
+    class Meta:
+        abstract = True
+
+
+class OrdenEntrada(Orden):
+    producto = models.OneToOneField(Producto, on_delete=models.PROTECT)
+    proveedor = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "Orden Entrada"
+
+    def  updateCantidadProducto(self):
+        self.producto.actualizarCantidad(self.cantidad_producto)
+
+    def save(self, *args, **kwargs):
+        self.updateCantidadProducto()
+        super().save(*args, **kwargs)
+
+    class Meta:
+       verbose_name_plural = "Ordenes de Entrada"
+
+
+class OrdenSalida(Orden):
+    producto = models.ForeignKey(Producto, on_delete=models.PROTECT)
+    ORDEN_ESTADOS = (
+        ('A', 'Activa'),
+        ('C', 'Cerrada'),
+        ('M', 'Modificada'),
+    )
+    estado = models.CharField(max_length=1, choices=ORDEN_ESTADOS)
+
+    def __str__(self):
+        return self.numero
+
+    def  updateCantidadProducto(self):
+        self.producto.actualizarCantidad((self.cantidad_producto * -1))
+
+    def save(self, *args, **kwargs):
+        self.updateCantidadProducto()
+        super().save(*args, **kwargs)
+
+    class Meta:
+       verbose_name_plural = "Ordenes de Salida"
+
+
+class OrdenDevolucion(Orden):
+    orden_salida = models.ForeignKey(OrdenSalida, verbose_name="Orden de Salida", on_delete=models.PROTECT) #.SET_DEFAULT
+
+    def __str__(self):
+        return self.numero
+
+    def  updateCantidadProducto(self):
+        self.orden_salida.producto.actualizarCantidad(self.cantidad_producto)
+
+    def save(self, *args, **kwargs):
+        self.updateCantidadProducto()
+        super().save(*args, **kwargs)
+
+    class Meta:
+       verbose_name_plural = "Ordenes de Devolucion"
